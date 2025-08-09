38c3d5f5c5e3a1486ee2025bcd127fb92b3acbf4
Kristian Hesselberg Brox <kristian.brox@gmail.com>
2025-08-09 12:46:36 +0200

    playing around

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use macroquad::miniquad::window::screen_size;
 use macroquad::prelude::*;
 
-#[macroquad::main("MyGame")]
+#[macroquad::main("HB_RUST")]
 async fn main() {
+    let size = screen_size();
+    print!("x: {}, y: {}", size.0.to_string(), size.1.to_string());
+    next_frame().await;
+
     loop {
-        clear_background(RED);
+        
+        clear_background(BLACK);
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
